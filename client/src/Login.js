356feafd7b058d9f880b265f37f66478f0145a87
Hreiðar Ingi <hreidar220@gmail.com>
2021-03-21 12:18:48 +0200

    added a button and styling

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,5 +6,14 @@ const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=889c3623b35c4fdb89d884dfbca8afba&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
 }
