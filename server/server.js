514489b80e75bdd1c05e93913fc803122f9c73dd
Hreiðar Ingi <hreidar220@gmail.com>
2021-03-21 13:20:19 +0200

    installed cors and some other changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const SpotifyApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -24,3 +26,5 @@ app.post("/login", (req, res) => {
       res.sendStatus(400);
     });
 });
+
+app.listen(3001);
