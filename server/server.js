68d0202524a97dff7ab6fa312de4d895b651217e
Hreiðar Ingi <hreidar220@gmail.com>
2021-03-23 20:20:52 +0200

    installed packages

--- server/server.js
+++ server/server.js
@@ -1,16 +1,44 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const SpotifyApi = require("spotify-web-api-node");
+const bodyParser = require("body-parser");
+const lyricsFinder = require("lyrics-finder");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
-  const spotifyApi = new SpotifWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "889c3623b35c4fdb89d884dfbca8afba",
-    clientSecret: "dbcd782fc94941de869f5294d9f6ff77",
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   });
 
   spotifyApi
@@ -18,13 +46,20 @@ app.post("/login", (req, res) => {
     .then((data) => {
       res.json({
         accessToken: data.body.access_token,
-        refresh_token: data.body.refresh_token,
-        expires_in: data.body.expires_in,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
       });
     })
-    .catch(() => {
+    .catch((err) => {
       res.sendStatus(400);
     });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({ lyrics });
 });
 
 app.listen(3001);
